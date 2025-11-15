073258ec359d09fd39a8f5e26f77db2a752bfb52
NikitaDolgopolovNd287 <nd287@cornell.edu>
2025-11-15 23:57:55 +0300

    wtf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors') //new
+const morgan = require('morgan')
 const path = require('path')
 
 // Middleware
